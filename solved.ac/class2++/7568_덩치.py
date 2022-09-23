6a1501761fac4b91086dc75f9c8075cb70636e13
HardM00N <baejaehyun95@gmail.com>
2022-09-23 09:25:56 +0900

    update

--- solved.ac/class2++/7568_덩치.py
+++ solved.ac/class2++/7568_덩치.py
@@ -12,9 +12,7 @@ for i in range(N):
     rank = 1
     for j in range(N):
         if data[i][0] < data[j][0] and data[i][1] < data[j][1]:
-
-            rank += 1
-    
+            rank += 1   
     print(rank, end=' ')
 
 '''
