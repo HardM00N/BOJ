2a674ad84fb3f14b3ae1821cdd762ac98b6284f2
HardM00N <baejaehyun95@gmail.com>
2022-10-17 10:00:31 +0900

    upload

--- solved.ac/class2++/10828_스택.py
+++ solved.ac/class2++/10828_스택.py
@@ -10,5 +10,36 @@
 - top : 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다. 
 '''
 
+import sys
+input = sys.stdin.readline
+
+stack = []
+
 for _ in range(int(input())):
-    
+    cmd = list(input().split())
+
+    if cmd[0] == 'push':
+        stack.append(int(cmd[1]))
+    elif cmd[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(stack))
+    elif cmd[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'top': 
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
+
+'''
+회고 / TIL
+- 스택의 기본 기능들을 문제 그대로 단순히 구현하면 쉽게 풀리는 문제였음. 
+- 다만 sys.stdin.readline으로 받지 않으면 시간 초과에 걸렸었음. 아직 손에 잘 안 붙지만 주의하자. 
+'''
