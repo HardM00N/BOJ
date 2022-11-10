1050ee7fa91cec357e7f0dba8c07477de221d7b9
HardM00N <baejaehyun95@gmail.com>
2022-11-10 12:11:18 +0900

    update

--- solved.ac/class2++/2805_나무 자르기.py
+++ solved.ac/class2++/2805_나무 자르기.py
@@ -15,13 +15,92 @@
 상근이는 환경에 매우 관심이 많기 때문에, 나무를 필요한 만큼만 집으로 가져가려고 한다. 
 이 때, 적어도 M미터의 나무를 집에 가져가기 위해서 절단기에 설정할 수 있는 높이의 최댓값을 구하는 프로그램을 작성하시오.
 '''
+
+# 첫 번째 풀이
 import sys
 input = sys.stdin.readline
 
 _, M = map(int, input().split())
-tree = list(map(int, input().split()))
+trees = list(map(int, input().split()))
 
-start, end = 1, max(tree)
+start = 1
+end = max(trees)                        # 이진 탐색은 가장 높은 나무까지 수행
 
 while start <= end:
+    mid = (start + end) // 2
+    total = 0                           # 나무를 잘라 얻은 합을 저장할 변수
     
+    for tree in trees:
+        if tree >= mid:                 # 나무를 잘랐을 때 결과가 0 이상일 때만 total에 저장
+            total += tree - mid         # 음수가 저장되면 올바른 연산이 안 되기 때문
+
+    if total < M:
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(end)
+
+
+
+# 두 번째 풀이
+import sys
+input = sys.stdin.readline
+from collections import Counter
+
+_, M = map(int, input().split())
+trees = Counter(map(int, input().split()))
+
+start = 1
+end = max(trees)
+
+while start <= end:
+    mid = (start + end) // 2
+    total = 0                               # 나무를 잘라 얻은 합을 저장할 변수
+    
+    for tree, i in trees.items():
+        if tree >= mid:                     # 나무를 잘랐을 때 결과가 0 이상일 때만 total에 저장
+            total += (tree - mid) * i       # 음수가 저장되면 올바른 연산이 안 되기 때문
+
+    if total < M:
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(end)
+
+'''
+회고 / TIL
+- 이진 탐색으로 풀었음에도 시간초과의 벽에 부딪혀서 한참을 고민했음. 
+- 불필요한 변수를 줄이니(result 필요없이 바로 end 출력) 아슬아슬하게 통과됨. (첫 번째 풀이, 4812ms)
+- 다른 사람의 풀이를 보니 Counter를 활용해 for문 내에서 획기적으로 시간을 줄였음. 
+- 이를 토대로, 두 번째 풀이를 작성해서 안전하게 통과함. (472ms)
+- 시간 제한이 1초인데, 첫 번째 풀이에서 4812ms가 어떻게 통과됐는지는 잘 모르겠음. 
+- 위의 두 개의 풀이에도 개선할 점이 여전히 있음. 리스트 컴프리헨션을 사용하면 시간을 더 단축할 수 있는데, 아직 익숙치가 않음.
+- 넘을 산이 많다...
+'''
+
+# 추가 풀이 (첫 번째 풀이에 리스트 컴프리헨션 적용)
+import sys
+input = sys.stdin.readline
+
+_, M = map(int, input().split())
+trees = list(map(int, input().split()))
+
+start = 1
+end = max(trees)                        # 이진 탐색은 가장 높은 나무까지 수행
+
+while start <= end:
+    mid = (start + end) // 2
+    total = sum(tree - mid for tree in trees if tree > mid)
+
+    if total < M:
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(end)
+
+'''
+첫 번째 풀이에 비해 시간이 획기적으로 줄긴 함 (4812ms -> 2888ms)
+'''
