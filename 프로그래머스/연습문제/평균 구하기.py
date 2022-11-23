f1cca86f4454478edf1908cfa1f5bbadc1073079
HardM00N <baejaehyun95@gmail.com>
2022-11-23 15:43:52 +0900

    update

---
+++ 프로그래머스/연습문제/평균 구하기.py
@@ -0,0 +1,16 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12944
+
+# 나의 풀이
+def solution(arr):
+    return sum(arr) / len(arr)      # 단순히 합과 개수로 나눠 평균 계산
+
+
+# 다른 사람의 풀이
+def average(list):
+    return (sum(list) / len(list))  # 좋아요 많은 풀이 가져왔는데, 저랑 같네요...
+
+'''
+회고 / TIL
+- 너무 쉬운 문제라 쓸 건 없음. 
+- 백준 위주로 풀어와서 프로그래머스가 적응이 안 됨...
+'''
