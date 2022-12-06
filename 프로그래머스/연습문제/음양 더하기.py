24c79fd9317141be5ac981368444bf9ac87d2073
HardM00N <baejaehyun95@gmail.com>
2022-12-06 11:29:50 +0900

    update

--- 프로그래머스/연습문제/음양 더하기.py
+++ 프로그래머스/연습문제/음양 더하기.py
@@ -2,5 +2,19 @@
 
 # 나의 풀이
 def solution(absolutes, signs):
-    answer = 123456789
-    return answer
+    
+    for idx, sign in enumerate(signs):
+        if not sign:                        # 부호가 음수면
+            absolutes[idx] *= -1            # 숫자를 음수로
+
+    return sum(absolutes)
+
+# 다른 사람의 풀이
+def solution(absolutes, signs):
+    return sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
+
+'''
+회고 / TIL
+- 푸는 로직은 모두 비슷함. 
+- 저런 한 줄로 푸는 스킬이 아직 부족함.
+'''
