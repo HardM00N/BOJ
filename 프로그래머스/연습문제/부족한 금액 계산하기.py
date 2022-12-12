3244ce00491f5e22343cce4f1bd387eb3482ad90
HardM00N <baejaehyun95@gmail.com>
2022-12-12 12:53:23 +0900

    update

--- 프로그래머스/연습문제/부족한 금액 계산하기.py
+++ 프로그래머스/연습문제/부족한 금액 계산하기.py
@@ -2,6 +2,22 @@
 
 # 나의 풀이
 def solution(price, money, count):
-    answer = -1
+    total = 0
+    
+    for i in range(1, count + 1):
+        total += price * i
+    
+    if money >= total:
+        return 0
+    else:
+        return total - money
 
-    return answer
+# 다른 사람의 풀이
+def solution(price, money, count):
+    return abs(min(money - sum([price * i for i in range(1, count + 1)]), 0))
+
+'''
+회고 / TIL
+- 위 두 풀이는 같은 내용이지만 아래가 훨씬 우아하다...
+- min으로 처리하는 부분이 인상적임.
+'''
