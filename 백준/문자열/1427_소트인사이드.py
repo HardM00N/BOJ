6b9a2730350cb5af89fece3cfc740baa1d2524e7
HardM00N <baejaehyun95@gmail.com>
2022-12-22 21:58:29 +0900

    update

---
+++ 백준/문자열/1427_소트인사이드.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/1427
+
+# 나의 풀이 (Python3 36ms)
+print(int(''.join(sorted(input(), reverse=True))))        # 문자열로 받아서 내림차순으로 정렬하고 int로 출력
+
+'''
+회고 / TIL
+- 쉬운 문제였음.
+- 다른 사람의 풀이도 비슷해서 생략함.
+'''
