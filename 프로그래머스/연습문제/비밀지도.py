e5ae926edfb110bfcd6fdf51e91d9b8357757fd7
HardM00N <baejaehyun95@gmail.com>
2022-12-29 18:16:29 +0900

    update

--- 프로그래머스/연습문제/비밀지도.py
+++ 프로그래머스/연습문제/비밀지도.py
@@ -1,2 +1,10 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/17681
 
+def solution(n, arr1, arr2):
+    answer = []
+    
+    for i in range(n):
+        or_res = format(arr1[i] | arr2[i], '0{}b'.format(n))
+        answer.append(or_res.replace('1', '#').replace('0', ' '))
+    
+    return answersdfdsdf
