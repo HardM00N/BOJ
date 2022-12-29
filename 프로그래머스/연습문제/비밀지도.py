ccc34a5473df9e8635f61843cdf8a31dada18365
HardM00N <baejaehyun95@gmail.com>
2022-12-29 18:16:49 +0900

    update

--- 프로그래머스/연습문제/비밀지도.py
+++ 프로그래머스/연습문제/비밀지도.py
@@ -7,4 +7,4 @@ def solution(n, arr1, arr2):
         or_res = format(arr1[i] | arr2[i], '0{}b'.format(n))
         answer.append(or_res.replace('1', '#').replace('0', ' '))
     
-    return answersdfdsdf
+    return answer
