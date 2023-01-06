2ff09b6057f33092abe90f575a380c9c8a6d2d85
HardM00N <baejaehyun95@gmail.com>
2023-01-06 19:00:05 +0900

    update

---
+++ 프로그래머스/연습문제/소수 만들기.py
@@ -0,0 +1,22 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12977
+
+# 나의 풀이
+from itertools import combinations
+
+def solution(nums):
+    prime_cnt = 0
+
+    for i in combinations(nums, 3):                     # 조합
+        num = sum(i)
+
+        for j in range(2, int(num ** 0.5) + 1):         # 소수 판별
+            if num % j == 0:
+                break
+        else:
+            prime_cnt += 1
+
+    return prime_cnt
+
+'''
+- 다들 고만고만한 풀이...
+'''
