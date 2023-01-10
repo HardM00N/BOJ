5818d8795a45c420829feb2e98375f470433cd89
HardM00N <baejaehyun95@gmail.com>
2023-01-11 00:00:36 +0900

    update

---
+++ 백준/분할정복/11582_치킨 TOP N.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/11582
+
+N = int(input())
+scores = list(map(int, input().split()))
+K = int(input())
+
+def merge_sort(N, scores, K):
+    if N == 1:
+        return scores
+    else:
+        N //= 2
+
+        
