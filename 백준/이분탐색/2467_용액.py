e1cde0e1361f707b94f6a705d13059dcc8e3df47
HardM00N <baejaehyun95@gmail.com>
2023-01-11 14:20:11 +0900

    update

--- 백준/이분탐색/2467_용액.py
+++ 백준/이분탐색/2467_용액.py
@@ -3,8 +3,8 @@
 N = int(input())
 sol = list(map(int, input().split()))
 
-res = int(1e9)
-min_left, min_right = 0, 0
+min = int(1e10)
+mins = []
 
 for i in range(N - 1):
     start = i + 1
@@ -14,17 +14,13 @@ for i in range(N - 1):
         mid = (start + end) // 2
         _sum = sol[i] + sol[mid]
         
-        if abs(_sum) < res:
-            res = abs(_sum)
-            min_left = i
-            min_right = mid
-
-            if _sum == 0:
-                break
+        if abs(_sum) < min:
+            min = abs(_sum)
+            mins = [sol[i], sol[mid]]
         
         if _sum < 0:
             start = mid + 1
         else:
             end = mid - 1
             
-print(sol[min_left], sol[min_right])
+print(*mins)
