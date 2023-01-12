e83c213de3c4b9cb05e8ba0371b08f4a28a6daf1
HardM00N <baejaehyun95@gmail.com>
2023-01-13 00:33:43 +0900

    update

--- 백준/DP/2156_포도주 시식.py
+++ 백준/DP/2156_포도주 시식.py
@@ -1,25 +1,28 @@
 # https://www.acmicpc.net/problem/2156
 
-# 나의 풀이 (Python3 48ms)
-# import sys
-# input = sys.stdin.readline
+# 나의 풀이 (Python3 44ms)
+import sys
+input = sys.stdin.readline
 
-# N = int(input())
-# wines = [int(input()) for _ in range(N)]
+N = int(input())
+wines = [int(input()) for _ in range(N)]
 
-# if N <= 2:
-#     print(sum(wines))
+if N <= 2:                                                                                      # N이 2 이하면 합이 최댓값
+    print(sum(wines))
 
-# else:
-#     dp = [0] * (N + 4)
-#     dp[0] = wines[0]
-#     dp[1] = dp[0] + wines[1]
+else:
+    dp = [0] * N
+    dp[0] = wines[0]
+    dp[1] = wines[0] + wines[1]
+    dp[2] = max(wines[0] + wines[1], wines[0] + wines[2], wines[1] + wines[2])                  # 3번째 와인을 포함하지 않은 case도 포함
 
-#     for i in range(2, N):
-#         dp[i] = max(
-#             wines[i] + dp[i - 2],
-#             wines[i] + wines[i - 1] + dp[i - 3],
-#             wines[i] + wines[i - 1] + dp[i - 4]
-#         )
+    for i in range(3, N):
+        dp[i] = max(dp[i - 1], wines[i] + dp[i - 2], wines[i] + wines[i - 1] + dp[i - 3])       # 마찬가지로 i번째 와인을 포함하지 않은 경우도 포함
 
-#     print(max(dp))
+    print(dp[-1])                                                                               # 마지막 값이 마실 수 있는 최댓값
+    
+'''
+회고 / TIL
+- class 3++의 계단 오르기와 매우 흡사하지만, i번째를 꼭 마시지 않아도 된다는 차이점이 있음.
+- 그 부분까지 고려해서 dp를 계산해나가야 함.
+'''
