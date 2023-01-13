62f44e919a353eb8f55fff54314cf984d90d0491
HardM00N <baejaehyun95@gmail.com>
2023-01-13 22:16:29 +0900

    update

---
+++ 백준/그리디/10162_전자레인지.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/10162
+
+# 나의 풀이 (Python3 40ms)
+T = int(input())
+
+A, B, C = 300, 60, 10
+
+if T % C:
+    print(-1)
+
+else:
+    for i in (A, B, C):
+        print(T // i, end=' ')
+        T %= i
+
+'''
+회고 / TIL
+- 가장 큰 수로 먼저 나눠주는 것이 최소한 버튼을 누르는 방법임.
+- 다른 사람들의 풀이도 비슷해 생략함.
+'''
