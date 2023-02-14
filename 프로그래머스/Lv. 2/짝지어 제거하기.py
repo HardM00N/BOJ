3bb00143d3c3fb2c617f7f5e846b0667122e948c
HardM00N <baejaehyun95@gmail.com>
2023-02-15 03:53:19 +0900

    update

--- 프로그래머스/Lv. 2/짝지어 제거하기.py
+++ 프로그래머스/Lv. 2/짝지어 제거하기.py
@@ -1,13 +1,18 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12973
 
-import re
-
+# 나의 풀이
 def solution(s):
-    while s:
-        temp = s
-        s = re.sub('([a-z])\\2{1,}', '', s)
+    stack = [s[0]]                          # 스택에 첫 글자 넣고 시작
+    
+    for i in s[1:]:                         # 두 번째 글짜부터 스택에 넣으면서
+        if stack and i == stack[-1]:        # 스택이 비어있지 않다면, 스택의 top과 비교
+            stack.pop()                     # 같다면, 짝지어 제거해야하므로 pop()
+        else:
+            stack.append(i)                 # 스택이 비었거나 스택의 top과 다르다면, append
+    
+    return 0 if stack else 1                # 스택이 비었으면 1, 차있으면 0
 
-        if s and temp == s:
-            return 0
-    else:
-        return 1
+'''
+회고 / TIL
+- 다른 사람의 풀이도 대체로 비슷함.
+'''
