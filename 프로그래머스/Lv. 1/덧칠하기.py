61fe9e7de65642236eda360376263575790082bd
HardM00N <baejaehyun95@gmail.com>
2023-04-02 22:05:21 +0900

    update

--- 프로그래머스/Lv. 1/덧칠하기.py
+++ 프로그래머스/Lv. 1/덧칠하기.py
@@ -1,6 +1,39 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/161989
 
-# 나의 풀이
+# 첫 번째 풀이 (시간 초과)
 def solution(n, m, section):
-    answer = 0
-    return answer
+    cnt = 0
+    wall = [0] * (n + 1)
+
+    for s in section:
+        wall[s] = 1
+    
+    for s in section:
+        wall[s:s + m] = [0] * m
+        cnt += 1
+
+        if not sum(wall):
+            break
+
+    return cnt
+
+# 두 번째 풀이
+def solution(n, m, section):
+    cnt = 0
+    wall = [0] * (n + 1)
+
+    for s in section:
+        wall[s] = 1
+    
+    for s in section:
+        wall[s:s + m] = [0] * m
+        cnt += 1
+
+        if not sum(wall):
+            break
+
+    return cnt
+
+print(solution(8, 4, [2, 3, 6]))
+print(solution(5, 4, [1, 3]))
+print(solution(4, 1, [1, 2, 3, 4]))
